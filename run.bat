e997e6a234c848f1147dfe9d1c11c61a0abdc5b8
Taterao Gaikwad <tateraog30@gmail.com>
2026-07-17 18:20:41 +0530

    Added latest code

---
+++ run.bat
@@ -0,0 +1,14 @@
+@echo off
+
+echo ==============================
+echo REST Assured Framework Started
+echo ==============================
+
+mvn clean test
+
+echo.
+echo ==============================
+echo Execution Completed
+echo ==============================
+
+pause
